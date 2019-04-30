b758d2ba2ac8c58ffe4bc96ca5080ae84112f3c2
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-04-30 10:56:44 -0700

    Implement index.js functionality to pass all tests

--- datastore/index.js
+++ datastore/index.js
@@ -32,33 +32,40 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, text) => {
+    if (err) {
+      let error = ('not a valid id');
+      callback(error);
+    } else {
+      callback(null, {id, text});
+    }
+  })
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
-};
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err) => {
+    if (err) {
+      callback('File does not exist');
+    } else {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if (err) {
+          callback (err);
+        } else {
+          callback(null, {id, text});
+        }
+      })
+    }
+  })
+}
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  })
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/todo', (req, res) => {
 // Read one (cRud) -- member route
 app.get('/todo/:id', (req, res) => {
   Todo.readOne(req.params.id, (err, todo) => {
+    console.log(todo);
     if (todo) {
       res.status(200).json(todo);
     } else {
